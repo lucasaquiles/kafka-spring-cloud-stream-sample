1184f86f1f2ed2b5b169388eaca23f4082bef425
Lucas Aquiles <lucasaquiles@gmail.com>
2019-12-11 14:43:03 -0300

    fix

--- publisher/src/main/java/com/lucasaquiles/publisher/controller/LocationController.java
+++ publisher/src/main/java/com/lucasaquiles/publisher/controller/LocationController.java
@@ -3,12 +3,13 @@ package com.lucasaquiles.publisher.controller;
 import com.lucasaquiles.publisher.controller.dto.LocationDTO;
 import com.lucasaquiles.publisher.controller.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.net.http.HttpResponse;
 
 @RestController("/location")
 public class LocationController {
@@ -17,10 +18,10 @@ public class LocationController {
     private LocationService locationService;
 
     @PostMapping
-    public LocationDTO addLocation(@Valid @RequestBody LocationDTO request) {
+    public ResponseEntity<LocationDTO> addLocation(@Valid @RequestBody LocationDTO request) {
 
         locationService.addLocation(request);
 
-        return HttpRespon;
+        return new ResponseEntity<>(request, HttpStatus.CREATED);
     }
 }
