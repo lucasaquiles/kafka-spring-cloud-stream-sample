846a78c442618ffd6a653309a68a4f5022a8c751
Lucas Aquiles <lucasaquiles@gmail.com>
2020-01-06 16:41:58 -0300

    socket server

---
+++ socket/src/main/kotlin/com/lucasaquiles/socket/SocketApplication.kt
@@ -0,0 +1,11 @@
+package com.lucasaquiles.socket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocketApplication>(*args)
+}
